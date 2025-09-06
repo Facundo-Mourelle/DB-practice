1f199b60d306ae31b228a812ed4f68534cdc88c3
Facundo Mourelle <facundo.mourelle@mi.unc.edu.ar>
2025-09-05 23:02:16 -0300

    esto es por mi propio bien

---
+++ practico2.sql
@@ -0,0 +1,62 @@
+CREATE TABLE IF NOT EXISTS country(
+	Code VARCHAR(150) PRIMARY KEY,
+	Name VARCHAR(150),
+	Continent VARCHAR(150),
+	Region VARCHAR(150),
+	SurfaceArea INT,
+	IndepYear INT,
+	Population INT,
+	LifeExpectancy INT,
+	GNP INT,
+	GNPOld INT,
+	LocalName VARCHAR(150),
+	GovernmentForm VARCHAR(150),
+	HeadOfState VARCHAR(150),
+	Capital INT,
+	Code2 VARCHAR(150)
+);
+
+CREATE TABLE IF NOT EXISTS city (
+	ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	Name VARCHAR(150),
+	CountryCode VARCHAR(150),
+	District VARCHAR(150),
+	Population INT,
+	FOREIGN KEY (CountryCode) REFERENCES country(Code)
+)
+ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS countrylanguage (
+	CountryCode VARCHAR(150) NOT NULL,
+	Language VARCHAR(150) NOT NULL,
+	IsOfficial VARCHAR(1),
+	Percentage DECIMAL(5,2),
+
+	FOREIGN KEY (CountryCode) REFERENCES country(Code),
+	PRIMARY KEY (CountryCode, Language)
+)
+ENGINE=InnoDB;
+
+source ~/Downloads/world-data.sql;
+
+CREATE TABLE IF NOT EXISTS continent (
+	ContinentName VARCHAR(150) NOT NULL PRIMARY KEY,
+	Area INT,
+	PercentageLand DECIMAL(5,2),
+	MostPopulousCity INT
+)
+ENGINE=InnoDB;
+
+INSERT INTO continent VALUES ('Africa', 30370000, 20.4, 608);
+INSERT INTO continent VALUES ('Antarctica', 14000000, 9.2, 4080);
+INSERT INTO continent VALUES ('Asia',44579000, 29.5,1024);
+INSERT INTO continent VALUES ('Europe',10180000, 6.8,3357);
+INSERT INTO continent VALUES ('North America', 24709000, 16.5, 2515);
+INSERT INTO continent VALUES ('Oceania',8600000, 5.9,130);
+INSERT INTO continent VALUES ('South America', 17840000, 12.0,206);
+
+-- Punto 6
+ALTER TABLE country
+ADD CONSTRAINT fk_country_continent
+FOREIGN KEY (Continent) REFERENCES continent(ContinentName);
+
