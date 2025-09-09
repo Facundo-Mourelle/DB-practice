2c1317202afa17d4d470ece669a60e263b2a4532
Facundo Mourelle <facundo.mourelle@mi.unc.edu.ar>
2025-09-08 23:45:48 -0300

    consulta 2

--- practico3/practico3.sql
+++ practico3/practico3.sql
@@ -1,14 +1,23 @@
+-- Ejercicio 1
+
 SELECT
     ci.Name AS CityName,
     co.Name AS CountryName,
     co.Region,
-    co.GovernmentForm
-FROM
-    city ci
+    co.GovernmentForm 
+FROM city ci 
 JOIN country co
-    ON (
-        ci.CountryCode = co.Code
-    )
-ORDER BY
-    ci.PopulationDESC
+ON (ci.CountryCode = co.Code)
+ORDER BY ci.Population DESC
+LIMIT 10;
+
+-- Ejercicio 2
+
+SELECT
+co.Name AS CountryName,
+ci.Name AS CityName
+FROM city ci
+JOIN country co
+ON (co.Capital = ci.ID)
+ORDER BY co.Population ASC
 LIMIT 10;
