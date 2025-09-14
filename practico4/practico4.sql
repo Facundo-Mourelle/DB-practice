dea97788370e758c03cf9d2c1761174b5797f520
Facundo Mourelle <facundo.mourelle@mi.unc.edu.ar>
2025-09-13 22:59:21 -0300

    ejercicio 2

--- practico4/practico4.sql
+++ practico4/practico4.sql
@@ -6,3 +6,13 @@ JOIN country co
 ON ci.CountryCode = co.Code 
 WHERE co.Population < 10000;
 
+-- Ejercicio 2
+
+WITH prom_city (promedio) AS (
+    SELECT avg(city2.Population) AS promedio
+    FROM city AS city2
+)
+SELECT city1.Name, city1.Population 
+FROM city AS city1 
+WHERE city1.Population > (SELECT promedio FROM prom_city) 
+ORDER BY city1.Population ASC;
