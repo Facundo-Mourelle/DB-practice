295daae864fc55c664869f5fc33c4d9de5e24985
Facundo Mourelle <facundo.mourelle@mi.unc.edu.ar>
2025-09-14 20:32:01 -0300

    ejercicio 3

--- practico4/practico4.sql
+++ practico4/practico4.sql
@@ -16,3 +16,18 @@ SELECT city1.Name, city1.Population
 FROM city AS city1 
 WHERE city1.Population > (SELECT promedio FROM prom_city) 
 ORDER BY city1.Population ASC;
+
+-- Ejercicio 3
+
+SELECT Name 
+FROM city AS c 
+WHERE Population >= SOME (
+    SELECT Population 
+    FROM country 
+    WHERE Continent = "Asia" 
+    )
+AND c.CountryCode NOT IN (
+    SELECT Code
+    FROM country 
+    WHERE Continent LIKE "Asia"
+);
