a04af03230e1a4bdae75e738552f5658f5c92287
Facundo Mourelle <facundo.mourelle@mi.unc.edu.ar>
2025-09-14 20:33:45 -0300

    añadida consignas como comentarios para mejorar comprension (y comodidad)

--- practico4/practico4.sql
+++ practico4/practico4.sql
@@ -1,4 +1,7 @@
 -- Ejercicio 1
+/*
+Listar el nombre de la ciudad y el nombre del país de todas las ciudades que pertenezcan a países con una población menor a 10000 habitantes.
+*/
 
 SELECT ci.Name, co.Name, ci.Population 
 FROM city ci 
@@ -7,6 +10,9 @@ ON ci.CountryCode = co.Code
 WHERE co.Population < 10000;
 
 -- Ejercicio 2
+/*
+Listar todas aquellas ciudades cuya población sea mayor que la población promedio entre todas las ciudades.
+*/
 
 WITH prom_city (promedio) AS (
     SELECT avg(city2.Population) AS promedio
@@ -18,6 +24,9 @@ WHERE city1.Population > (SELECT promedio FROM prom_city)
 ORDER BY city1.Population ASC;
 
 -- Ejercicio 3
+/*
+Listar todas aquellas ciudades no asiáticas cuya población sea igual o mayor a la población total de algún país de Asia.
+*/
 
 SELECT Name 
 FROM city AS c 
@@ -31,3 +40,9 @@ AND c.CountryCode NOT IN (
     FROM country 
     WHERE Continent LIKE "Asia"
 );
+
+-- Ejercicio 4
+/*
+Listar aquellos países junto a sus idiomas no oficiales, que superen en porcentaje de hablantes a cada uno de los idiomas oficiales del país.
+*/
+
