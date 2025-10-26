e8a2634fd4a06aedfe99ed2d1bbb8aff7c8aa604
Facundo Mourelle <facundo.mourelle@mi.unc.edu.ar>
2025-10-26 20:44:03 -0300

    ejercicio 6

--- practico7/practico7.js
+++ practico7/practico7.js
@@ -151,7 +151,21 @@ db.movies.find(
 // EJERCICIO 6
 // Listar el id del teatro (theaterId), estado (“location.address.state”), ciudad (“location.address.city”), y coordenadas (“location.geo.coordinates”) de los teatros que se encuentran en algunos de los estados "CA", "NY", "TX" y el nombre de la ciudades comienza con una ‘F’. Listar ordenados por estado y ciudad.
 
-
+db.theaters.find(
+    {
+        "location.address.state": {$in: ["CA", "NY", "TX"]},
+        "location.address.city": {$regex: "^F"}
+    },
+    {
+        "theaterId": 1,
+        "location.address.state": 1,
+        "location.address.city": 1,
+        "location.geo.coordinates": 1
+    }
+).sort(
+        {"location.address.state": 1},
+        {"location.address.city": 1},
+    )
 
 
 // ======================================================
