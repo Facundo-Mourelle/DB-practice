fd9847a57962268902075c33ea855783e226217e
Facundo Mourelle <facundo.mourelle@mi.unc.edu.ar>
2025-10-26 19:39:40 -0300

    ejercicio 4

--- practico7/practico7.js
+++ practico7/practico7.js
@@ -114,6 +114,16 @@ db.comments.find(
 // EJERCICIO 4
 // Listar el nombre, id de la película, texto y fecha de los 3 comentarios más recientes realizados por el usuario con email patricia_good@fakegmail.com
 
+db.comments.find(
+    {
+        "email": {$eq: "patricia_good@fakegmail.com"},
+    },
+    {
+        _id: 0, name: 1, movie_id: 1, text: 1, date: 1
+    }
+).sort({"date": -1}
+).limit(3)
+
 
 // ======================================================
 // EJERCICIO 5
