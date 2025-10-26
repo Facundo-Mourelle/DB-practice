26ca3ce74b66831fcd999fe062d4287cb6c6ae9d
Facundo Mourelle <facundo.mourelle@mi.unc.edu.ar>
2025-10-26 20:34:14 -0300

    ejercicio 5

--- practico7/practico7.js
+++ practico7/practico7.js
@@ -129,7 +129,23 @@ db.comments.find(
 // EJERCICIO 5
 // Listar el título, idiomas (languages), géneros, fecha de lanzamiento (released) y número de votos (“imdb.votes”) de las películas de géneros Drama y Action (la película puede tener otros géneros adicionales), que solo están disponibles en un único idioma y por último tengan un rating (“imdb.rating”) mayor a 9 o bien tengan una duración (runtime) de al menos 180 minutos. Listar ordenados por fecha de lanzamiento y número de votos
 
-
+db.movies.find(
+    {
+        genres: {$all: ["Drama", "Action"]},
+        languages: {$size: 1},
+        $or: [
+            {"imdb.rating": {$type: "double", $gt: 9}},
+            {"runtime": {$gte: 180}}
+        ],
+    },
+    {
+        title: 1, languages: 1, genres: 1, released: 1, "imdb.votes": 1
+    }
+).sort(
+        {"released": -1},
+        {"imdb.votes": -1},
+        {_id: 1}
+    )
 
 // ======================================================
 // EJERCICIO 6
