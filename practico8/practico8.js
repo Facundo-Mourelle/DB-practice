9fba1b052c245b8c937eee9bcdd1bc594051e2cd
Facundo Mourelle <facundo.mourelle@mi.unc.edu.ar>
2025-10-27 16:13:40 -0300

    ejercicio 12a

--- practico8/practico8.js
+++ practico8/practico8.js
@@ -363,6 +363,30 @@ db.comments.aggregate([
 // Listar el id y nombre de los restaurantes junto con su puntuación máxima, mínima y la suma total. Se puede asumir que el restaurant_id es único.
 // a) Resolver con $group y accumulators.
 
+db.restaurants.aggregate([
+    {
+        $unwind: "$grades"
+    },
+    {
+        $group: {
+            _id: "$restaurant_id",
+            name: {$first: "$name"},
+            max: {$max: "$grades.score"},
+            min: {$min: "$grades.score"},
+            total: {$sum: "$grades.score"}
+        }
+    },
+    {
+        $project: {
+            _id: 0,
+            restaurant_id: "$_id",
+            name: "$name",
+            max: 1,
+            min: 1,
+            total: 1
+        }
+    }
+])
 
 // b) Resolver con expresiones sobre arreglos (por ejemplo, $sum) pero sin $group.
 
