7d22bab79bc94bf6f0c4c6e9baf6c296796e7114
Facundo Mourelle <facundo.mourelle@mi.unc.edu.ar>
2025-10-26 21:22:41 -0300

    ejercicio 11

--- practico7/practico7.js
+++ practico7/practico7.js
@@ -273,4 +273,25 @@ db.restaurants.find(
 	"grade" : "A",
 	"score" : 21
 }
- */
+*/
+
+db.restaurants.updateOne(
+    {restaurant_id: "50018608"},
+    {
+        $push: {
+            grades: {
+                $each: [{
+                    "date" : ISODate("2020-02-25T00:00:00Z"),
+                	"grade" : "A",
+                	"score" : 21
+                },
+                {
+                	"date" : ISODate("2019-10-10T00:00:00Z"),
+                	"grade" : "A",
+                	"score" : 18
+                }
+                ]
+            }
+        }
+    }
+)
